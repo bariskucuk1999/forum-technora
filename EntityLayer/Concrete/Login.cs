9a7d8c90b8c61c41e234e51fd4d432250de42fbd
bariskucuk1999 <bbariskucuk@outlook.com>
2021-10-23 23:23:06 +0300

    Test update

---
+++ EntityLayer/Concrete/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        [StringLength(16)]
+        public string Username { get; set; }
+        [StringLength(20)]
+        public string Password { get; set; }
+    }
+}
